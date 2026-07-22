a400caa9a0df130c37d6886f1f28fa3088b29e84
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-07-22 15:53:24 -0300

    feat(globalfooter): add responsive footer

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -250,3 +250,50 @@ const ghSubmenuHoverSync = () => {
 
 document.addEventListener('DOMContentLoaded', ghSubmenuHoverSync)
 ghHoverCapable.addEventListener('change', ghSubmenuHoverSync)
+
+// ==================================================
+// Global Footer
+// ==================================================
+
+// Global Footer - Directory
+// ==================================================
+
+class GlobalFooterDirectory {
+  constructor() {
+    this.gfDirectory = document.getElementById('globalfooter-directory')
+    this.gfDirectoryButtons = this.gfDirectory.querySelectorAll('.globalfooter-directory-section-title-button')
+    this.viewport = window.matchMedia('(max-width: 767px)')
+    this.bindClicks()
+    this.init(this.viewport)
+    this.viewport.addEventListener('change', evt => this.init(evt))
+  }
+
+  bindClicks() {
+    this.gfDirectoryButtons.forEach(btn => {
+      btn.addEventListener('click', () => {
+        if (btn.disabled) return
+        const isExpanded = btn.getAttribute('aria-expanded') === 'true'
+        btn.setAttribute('aria-expanded', String(!isExpanded))
+        btn.parentElement.parentElement.classList.toggle('globalfooter-directory-section-expanded', !isExpanded)
+      })
+    })
+  }
+
+  init(evt) {
+    this.gfDirectoryButtons.forEach(btn => {
+      btn.querySelector('.globalfooter-directory-section-title-button-icon').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M297.4 438.6C309.9 451.1 330.2 451.1 342.7 438.6L502.7 278.6C515.2 266.1 515.2 245.8 502.7 233.3C490.2 220.8 469.9 220.8 457.4 233.3L320 370.7L182.6 233.4C170.1 220.9 149.8 220.9 137.3 233.4C124.8 245.9 124.8 266.2 137.3 278.7L297.3 438.7z"/></svg>'
+      if (evt.matches) {
+        btn.removeAttribute('disabled')
+        btn.setAttribute('aria-expanded', 'false')
+        btn.setAttribute('aria-controls', btn.parentElement.nextElementSibling.getAttribute('id'))
+      } else {
+        btn.setAttribute('disabled', '')
+        btn.removeAttribute('aria-expanded')
+        btn.removeAttribute('aria-controls')
+        btn.parentElement.parentElement.classList.remove('globalfooter-directory-section-expanded')
+      }
+    })
+  }
+}
+
+new GlobalFooterDirectory()
